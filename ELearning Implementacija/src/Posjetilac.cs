52b328a9bcdfd1ec6da1ae8ddd50d03d7056162f
Adnan <asabanovic7@etf.unsa.ba>
2020-05-15 20:32:44 +0100

    Dodao klasu Posjetilac, bez ikakve funkcionalnosti

---
+++ ELearning Implementacija/src/Posjetilac.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Posjetilac
+{
+	int guestID;
+
+    public Posjetilac(int guestID) => this.guestID = guestID;
+
+    public void prijaviSe()
+    {
+        // pass
+    }
+
+    public void registracija()
+    {
+        // pass
+    }
+
+    public int getGuestID()
+    {
+        return guestID;
+    }
+
+    public void setGuestID(int guestID)
+    {
+        this.guestID = guestID;
+    }
+
+}
