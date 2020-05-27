bd97d3007d90955f0ed2583301850ecf37b07bff
Adnan <asabanovic7@etf.unsa.ba>
2020-05-27 20:41:40 +0100

    Forma za registraciju dodana

--- ELearning Implementacija/E-Learning/Controllers/NalogController.cs
+++ ELearning Implementacija/E-Learning/Controllers/NalogController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using E_Learning.Models;
 using E_Learning.Data;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore;
 
 namespace E_Learning.Controllers
 {
@@ -32,6 +34,7 @@ namespace E_Learning.Controllers
             Console.WriteLine(Email);
             Console.WriteLine(Password);
             */
+
             return RedirectToAction("Index", "Home");
         }
 
@@ -45,6 +48,21 @@ namespace E_Learning.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Registracija([Bind("Id,Ime,Prezime,Email,Password")] Korisnik korisnik)
+        {
+            if (ModelState.IsValid)
+            {
+                if(await _context.Korisnik.FirstOrDefaultAsync(k => k.Email == korisnik.Email) != null) return View();
+                _context.Add(korisnik);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
         }
     }
 }
