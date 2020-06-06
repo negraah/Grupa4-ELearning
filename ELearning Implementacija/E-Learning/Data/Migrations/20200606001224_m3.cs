ed86805d2d2ee410a77460f3dcf276347bf960d3
Adnan <asabanovic7@etf.unsa.ba>
2020-06-06 01:17:55 +0100

    Promjena baze

---
+++ ELearning Implementacija/E-Learning/Data/Migrations/20200606001224_m3.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace E_Learning.Data.Migrations
+{
+    public partial class m3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ZadnjiDaily",
+                table: "Korisnik",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ZadnjiDaily",
+                table: "Korisnik");
+        }
+    }
+}
